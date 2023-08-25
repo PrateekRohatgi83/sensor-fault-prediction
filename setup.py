82ead5cf74a0086e8346356ea3c9f62aa960e313
PrateekRohatgi83 <rohatgiprateek2014@gmail.com>
2023-08-25 18:41:48 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "sensor",
+    version="0.0.1",
+    author="prateek rohatgi",
+    author_email="rohatgiprateek2014@gmail.com",
+    packages=find_packages(),
+)
