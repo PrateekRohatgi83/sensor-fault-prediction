ade15316b32116c4396fef7bb990879f8510d09d
PrateekRohatgi83 <rohatgiprateek2014@gmail.com>
2023-08-31 13:13:47 +0530

    data pipeline

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
 from sensor.exception import SensorException
+from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig
 import os, sys
 from sensor.logger import logging
 
-def test_exception():
-    try:
-        logging.info("test exception")
-        x = 1/0
-    except Exception as e:
-        raise SensorException(e, sys)
-
 if __name__ == '__main__':
-    try:
-        test_exception()
-    except Exception as e:
-        print (e)
-
-    # mongodb_client = MongoDBClient()
-    # print("collection name: ", mongodb_client.database.list_collection_names())
+   training_pipeline_config = TrainingPipelineConfig()
+   data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+   print(data_ingestion_config.__dict__)
--- sensor/constant/training_pipeline/__init__.py
+++ sensor/constant/training_pipeline/__init__.py
@@ -13,3 +13,12 @@ PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 MODEL_FILE_NAME = "model.pkl"
 SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 SCHEMA_DROP_COLS = "drop_columns"
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_COLLECTION_NAME: str = "sensor"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
